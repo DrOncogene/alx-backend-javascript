c10f3ab411890fdfd3dbc98859338b4e4326f693
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-23 23:22:49 +0100

    resolving and rejecting a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,14 @@
 export default function getResponseFromAPI(success) {
-  const promise = new Promise(() => {});
-  if (success) {
-    return promise.then({
-      status: 200,
-      body: 'Success',
-    });
-  }
-  return promise.catch('The fake API is not working correctly');
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working correctly'));
+    }
+  });
+
+  return promise;
 }
