09130be5fd09ebbfea05ff6baeaf56e17634da74
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-24 17:42:29 +0100

    wahala: attaching handlers to a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
-export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
+export default async function handleResponseFromAPI(promise) {
+  const newPromise = promise.then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
+
+  return newPromise;
 }
