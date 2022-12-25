eb48e53c63112d2fd49e7957028a8c1e85109498
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-25 12:08:04 +0100

    handling multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(fName, lName, fileName) {
+  return Promise.all([signUpUser(fName, lName), uploadPhoto(fileName)])
+    .then((res) => res)
+    .catch((err) => err);
+}
