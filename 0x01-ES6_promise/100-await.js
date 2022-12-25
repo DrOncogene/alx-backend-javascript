2512756fa8bcf68f8b9c7c6ea27a37fbf201f8c2
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-25 20:34:36 +0100

    async/await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   return {
     photo: await uploadPhoto()
       .then((res) => res)
-      .catch(() => null),
+      .catch((err) => null),
     user: await createUser()
       .then((res) => res)
-      .catch(() => null),
+      .catch((err) => null),
   };
 }
