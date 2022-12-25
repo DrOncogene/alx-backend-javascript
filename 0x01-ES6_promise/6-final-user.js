96dc3fa2408f5cf36d544d91577b944d38251575
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-25 19:52:05 +0100

    handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(fName, lName, fileName) {
-  return Promise.all([signUpUser(fName, lName), uploadPhoto(fileName).catch((err) => err)]);
+  // const res = Promise.all([signUpUser(fName, lName), uploadPhoto(fileName).catch((err) => err)])
+  //   .then((res) => res);
+  const res1 = signUpUser(fName, lName).then((res) => ({ status: 'fulfilled', value: res }));
+  const res2 = uploadPhoto(fileName).catch((err) => ({ status: 'rejected', value: `Error: ${err.message}` }));
+  const prom = Promise.all([res1, res2]);
+
+  return prom;
 }
