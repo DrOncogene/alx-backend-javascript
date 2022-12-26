0e2c8f3d5558b718275289f43c02e84c97815df7
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-26 06:59:55 +0100

    async/await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,12 +2,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return {
-    photo: await uploadPhoto()
-      .then((res) => res)
-      .catch((err) => null),
-    user: await createUser()
-      .then((res) => res)
-      .catch((err) => null),
-  };
+  let obj;
+  try {
+    obj = {
+      photo: await uploadPhoto().then((res) => res),
+      user: await createUser().then((res) => res),
+    };
+  } catch (err) {
+    obj = { photo: null, user: null };
+  }
+  if (!obj.photo || !obj.user) return { photo: null, user: null };
+
+  return obj;
 }
