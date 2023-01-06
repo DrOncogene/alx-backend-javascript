91c513f150eba56ef5672961e162cf81b392426c
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-06 12:48:50 +0100

    tasks 6 and 7

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,42 @@ interface Teacher {
   yearsOfExperience?: number,
   location: string
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+type printTeacherFunction = (firstName: string, lastName: string) => string;
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework():  string;
+  displayName():  string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+const StudentClass: StudentConstructor = 
+  class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+
+    workOnHomework() {
+      return 'Currently working';
+    }
+    displayName() {
+      return this.firstName;
+    }
+  };
