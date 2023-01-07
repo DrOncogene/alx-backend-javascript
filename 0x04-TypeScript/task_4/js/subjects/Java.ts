b806bfa3377ec39b6d5f5837e0756d84734e586d
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-07 04:34:17 +0100

    task 9: namespace merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,18 @@
+/// <reference path="Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Jave';
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingJava <= 0) {
+        return 'No available teacher'
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
