3522b1539658459e8b8056ab50ee4565b0a8b8d1
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-05-20 18:31:49 +0100

    task 9: more integration test, using .deep

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
@@ -9,6 +10,20 @@ app.get('/', (req, res) => {
 app.get('/cart/:id(\\d+)', (req, res) => {
   const id = req.params.id;
   res.status(200).send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.userName;
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(7865, 'localhost', () => {
