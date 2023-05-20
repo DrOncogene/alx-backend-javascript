17c37838e1d53972e907663864a957f4a31fd6c4
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-05-20 16:32:11 +0100

    task 7: integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('payment api', () => {
+  const URL = 'http://localhost:7865/';
+  it('test response status code is 200', () => {
+    request.get(URL, (err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+    });
+  });
+  it('test response data is correct', () => {
+    request.get(URL, (err, res) => {
+      expect(res.body).to.be.equal('Welcome to the payment system');
+    });
+  });
+});
